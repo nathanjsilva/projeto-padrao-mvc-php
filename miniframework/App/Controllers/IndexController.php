886ee750999218684728bcdd4963826da68324eb
Nathan Silva <nathan.ads.100@gmail.com>
2021-08-01 00:21:50 -0300

    feature: configurando chamada dos arquivos de view no controller

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -4,13 +4,32 @@ namespace App\Controllers;
 
 class IndexController {
 
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+
+    public function render($view){
+
+        $classeAtual = get_class($this);
+        $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+
+        require_once "../App/Views/".$classeAtual."/".$view.".phtml";
+    }
+
 	public function index() {
-		echo 'Chegamos ao IndexController e disparamentos a action index';
+        $this->view->dados = array('sofa', 'cadeira', 'cama', 'mesa');
+        $this-> render("index");
 	}
 
 	public function sobreNos() {
-		echo 'Chegamos ao IndexController e disparamentos a action sobreNos';
+        $this->view->dados = array('sofa', 'cadeira', 'cama', 'mesa');
+        $this-> render("index");
 	}
+
 }
 
 
